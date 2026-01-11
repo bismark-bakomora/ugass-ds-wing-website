a586910a5797f8cd2032018bf5f8017e55ea35d4
bismark-bakomora <bismarkzebill@gmail.com>
2026-01-11 16:15:59 +0000

    configure tailwind css for react and typescript (vite)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ugassGreen: "#0F3D2E",
+        ugassGold: "#C9A227"
+      }
+    }
+  },
+  plugins: [],
+};
